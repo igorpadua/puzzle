ab051fe05575ec609dcc661cd3a5748b7e60f12f
igorpadua <igorpadua@tutanota.com>
2023-09-28 15:38:53 -0300

    feat: classe puzzle com seus atributos e métodos

---
+++ puzzle.cpp
@@ -0,0 +1,21 @@
+#include "puzzle.hpp"
+
+Puzzle::Puzzle()
+{
+
+}
+
+void Puzzle::run()
+{
+
+}
+
+void Puzzle::events()
+{
+
+}
+
+void Puzzle::draw()
+{
+
+}
---
+++ puzzle.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <array>
+
+class Puzzle
+{
+public:
+    Puzzle();
+    void run();
+
+protected:
+    void events();
+    void draw();
+
+private:
+    std::unique_ptr<sf::RenderWindow> m_window;
+    std::unique_ptr<sf::Event> m_event;
+
+    sf::Texture m_texture;
+    int m_w, m_n, m_x, m_y, m_dx, m_dy;
+    std::array<std::array<int, 6>, 6> grid;
+    sf::Sprite m_sprite;
+};
+
